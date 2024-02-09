8bc62464e5c261458c644006c4b1f15c53ef32a7
Guilherme Viana <guibviana@icloud.com>
2024-02-09 18:35:28 +0000

    added styling method to change balance amount design

--- bankey/bankey/Main/AccountSummary/AccountSummaryTableViewCellView.swift
+++ bankey/bankey/Main/AccountSummary/AccountSummaryTableViewCellView.swift
@@ -62,9 +62,9 @@ class AccountSummaryTableViewCellView: UIView {
 	lazy var balanceAmountLabel: UILabel = {
 		let label = UILabel()
 		label.translatesAutoresizingMaskIntoConstraints = false
-		label.text = "$17,562.44"
 		label.textAlignment = .right
 		label.font = UIFont.preferredFont(forTextStyle: .body)
+		label.attributedText = makeFormattedBalance(dollars: "17,562", cents: "44")
 		return label
 	}()
 	
@@ -106,5 +106,22 @@ extension AccountSummaryTableViewCellView {
 			balanceStackView.centerYAnchor.constraint(equalTo: centerYAnchor),
 			balanceStackView.trailingAnchor.constraint(equalTo: chevronImageView.leadingAnchor, constant: -15)
 		])
+	}
+}
+
+extension AccountSummaryTableViewCellView {
+	private func makeFormattedBalance(dollars: String, cents: String) -> NSMutableAttributedString {
+			let dollarSignAttributes: [NSAttributedString.Key: Any] = [.font: UIFont.preferredFont(forTextStyle: .callout), .baselineOffset: 8]
+			let dollarAttributes: [NSAttributedString.Key: Any] = [.font: UIFont.preferredFont(forTextStyle: .title1)]
+			let centAttributes: [NSAttributedString.Key: Any] = [.font: UIFont.preferredFont(forTextStyle: .footnote), .baselineOffset: 8]
+			
+			let rootString = NSMutableAttributedString(string: "$", attributes: dollarSignAttributes)
+			let dollarString = NSAttributedString(string: dollars, attributes: dollarAttributes)
+			let centString = NSAttributedString(string: cents, attributes: centAttributes)
+			
+			rootString.append(dollarString)
+			rootString.append(centString)
+			
+			return rootString
 	}
 }
